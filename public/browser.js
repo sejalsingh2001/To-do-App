c80548244d99084cc0508d874eac1d3059ae8919
Sejal <sejalsingh913@gmail.com>
2021-09-03 10:37:35 +0530

    to-do-app
    to-do-app

---
+++ public/browser.js
@@ -0,0 +1,26 @@
+
+
+document.addEventListener("click", function(e) {
+  //DELETE FEATURE
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Do you really want to delete this item permanently?")) {
+      axios.post('/delete-item', {id: e.target.getAttribute("data-id")}).then(function () {
+        e.target.parentElement.parentElement.remove()
+      }).catch(function() {
+        console.log("Please try again later.")
+      })
+    }
+  }
+  
+    //UPDATE FEATURE
+    if (e.target.classList.contains("edit-me")) {
+      let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+      if (userInput) {
+        axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function () {
+          e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
+        }).catch(function() {
+          console.log("Please try again later.")
+        })
+      }
+    }
+  })
